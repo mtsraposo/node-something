5ec981c01cd8ac452f624b16374cf02abf607cdb
Matheus Raposo <matheus_raposo@hotmail.com>
2023-07-22 22:10:19 -0300

    add rolling window streams

--- src/integrations/constants.js
+++ src/integrations/constants.js
@@ -1,3 +1,3 @@
-export const BINANCE_STREAMS = ['btcusdt@ticker'];
+export const BINANCE_STREAMS = ['btcusdt@ticker_1h', 'btcusdt@ticker_4h'];
 export const BINANCE_WEBSOCKET_STREAM_URL = 'wss://testnet.binance.vision/stream';
 export const BINANCE_WEBSOCKET_API_URL = 'wss://testnet.binance.vision/ws-api/v3';
