36eeea555d505a2525c25b58b72e737fa3fcf4fa
Matheus Raposo <matheus_raposo@hotmail.com>
2023-08-03 21:51:02 -0300

    config sequelize in js and use env vars for prod

---
+++ config/config.js
@@ -0,0 +1,31 @@
+const { config } = require('dotenv');
+const { env } = require('src/env.js');
+
+config();
+
+module.exports = {
+    dev: {
+        username: 'postgres',
+        password: 'postgres',
+        database: 'node_something_dev',
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+    },
+    test: {
+        username: 'postgres',
+        password: 'postgres',
+        database: 'node_something_test',
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+    },
+    prod: {
+        username: env.postgres.username,
+        password: env.postgres.password,
+        database: 'node_something',
+        host: env.postgres.host,
+        dialect: 'postgres',
+        port: env.postgres.port,
+    },
+};
