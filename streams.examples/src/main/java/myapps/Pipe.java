23e3c4cfb82cae8481359c2578d8da3fadf4106b
Matheus Raposo <matheus_raposo@hotmail.com>
2023-08-12 15:19:37 -0300

    add kstreams app skeleton

---
+++ streams.examples/src/main/java/myapps/Pipe.java
@@ -0,0 +1,23 @@
+package myapps;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Properties;
+
+public class Pipe {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        final StreamsBuilder builder = new StreamsBuilder();
+        builder.stream("dev.binance.quote.received.v1.json").to("dev.binance.quote.processed.v1.json");
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+    }
+}
